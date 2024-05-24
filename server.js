d1f1b4353148217ee52e5d77e0c9e6e225bdeefa
danh truong <danhtruongwa@yahoo.com>
2024-05-23 21:52:25 -0700

    set up config.env with mongoose url, installed mongoose, connect danhtour database

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const app = require("./app");
+
 dotenv.config({ path: "./config.env" });
 
-const app = require("./app");
+//replace password in database url
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+//connect to database
+mongoose.connect(DB, {}).then(() => {
+  console.log("DB Danhtours connected");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
