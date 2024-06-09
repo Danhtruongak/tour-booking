5a6b6a4dbe296d5e6854a228a5c776e0dfd222ad
danh truong <danhtruongwa@yahoo.com>
2024-06-09 00:00:14 -0700

    revert routes/tours

--- routes/tours.js
+++ routes/tours.js
@@ -1,16 +1,11 @@
+//routes/tour
 const express = require("express");
 const toursDAOS = require("../daos/tours");
 const router = express.Router();
 const authDAOS = require("../daos/auth");
 
-////Mounting a router /tour routes/////////////////////////
-
 router.route("/").get(toursDAOS.getAllTours).post(toursDAOS.createTour);
 router.get("/search", toursDAOS.searchTours);
-router
-  .route("/:id")
-  .get(toursDAOS.getTour)
-  .patch(toursDAOS.updateTour)
-  .delete(authDAOS.protect, authDAOS.restrictTo("admin"), toursDAOS.deleteTour);
+router.route("/:id").get(toursDAOS.getTour);
 
 module.exports = router;
